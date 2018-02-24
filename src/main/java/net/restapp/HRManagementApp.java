d2a7673d69222ddbad94426abf729b74854e0d7f
mexmatanka <svitlana.anulich@gmail.com>
2018-02-24 19:16:01 +0200

    Create project structure with two aplication.properties files and add one of them(application-dev.properties) to the .gitignore

---
+++ src/main/java/net/restapp/HRManagementApp.java
@@ -0,0 +1,12 @@
+package net.restapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HRManagementApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HRManagementApp.class, args);
+    }
+}
