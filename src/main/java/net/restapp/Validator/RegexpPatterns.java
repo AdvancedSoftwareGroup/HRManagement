4af19c39109b056e8acfde020e8ab9a57ab87b07
kurotkin <kurotkin@users.noreply.github.com>
2018-03-01 11:11:57 +0300

    Correction of Service Symbols in RegexpPatterns

--- src/main/java/net/restapp/Validator/RegexpPatterns.java
+++ src/main/java/net/restapp/Validator/RegexpPatterns.java
@@ -5,8 +5,4 @@ public class RegexpPatterns {
     public static final String patternStringWithNumbersLettersAndDash="^\\s*[\\da-zA-Z][\\da-zA-Z\\s][\\d-a-z-A-Z\\s]*$";
     public static final String messageStringWithNumbersLettersAndDash="This field must contains only numbers, letters and dash";
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> master
