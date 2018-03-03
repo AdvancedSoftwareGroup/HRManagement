baca6033b5fbaedb79ef18be48eff37bbd983e4b
mexmatanka <svitlana.anulich@gmail.com>
2018-03-03 22:08:17 +0200

    send error body with HTTPStatus

--- src/main/java/net/restapp/restcontroller/DepartmentController.java
+++ src/main/java/net/restapp/restcontroller/DepartmentController.java
@@ -1,17 +1,14 @@
 package net.restapp.restcontroller;
 
+import net.restapp.exception.ErrorBody;
 import net.restapp.model.Department;
 import net.restapp.servise.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import javax.validation.Valid;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/department")
@@ -21,54 +18,66 @@ public class DepartmentController {
     DepartmentService departmentService;
 
     @RequestMapping(value = "/{departmentId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Department> getDepartment(@PathVariable("departmentId") Long departmentId){
+    public ResponseEntity<Object> getDepartment(@PathVariable("departmentId") Long departmentId,HttpServletRequest request){
+
         if (departmentId == null){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            ErrorBody errorBody = new ErrorBody(
+                    HttpStatus.BAD_REQUEST,
+                    getClass(),
+                    "departmentId must not be equals null",
+                    request);
+            return new ResponseEntity<>(errorBody, HttpStatus.BAD_REQUEST);
         }
         Department department =  departmentService.getById(departmentId);
 
         if (department == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            ErrorBody errorBody = new ErrorBody(
+                    HttpStatus.NOT_FOUND,
+                    getClass(),
+                    "Not found element with id="+departmentId,
+                    request);
+            return new ResponseEntity<>(errorBody, HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(department, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/{departmentId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Department> deleteDepartment(@PathVariable("departmentId") Long departmentId){
-        if (departmentId == null){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        Department department = departmentService.getById(departmentId);
-        if (department == null){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        departmentService.delete(departmentId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-
-    @RequestMapping(value = "/getAll", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<List<Department>> getAllDepartment(){
-        List<Department> departments = departmentService.getAll();
-        if (departments.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(departments,HttpStatus.OK);
-    }
-
-
-    @RequestMapping(value = "/add", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Department> saveDepartment(@RequestBody @Valid Department department, UriComponentsBuilder builder){
-        HttpHeaders httpHeaders = new HttpHeaders();
-
-        if (department == null){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        departmentService.save(department);
-
-        httpHeaders.setLocation(builder.path("/department/getAll").buildAndExpand().toUri());
-        return new ResponseEntity<>(department,httpHeaders,HttpStatus.CREATED);
-    }
+//    @RequestMapping(value = "/{departmentId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//    public ResponseEntity<Department> deleteDepartment(@PathVariable("departmentId") Long departmentId){
+//        if (departmentId == null){
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+//        Department department = departmentService.getById(departmentId);
+//        if (department == null){
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//        departmentService.delete(departmentId);
+//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//    }
+//
+//
+//    @RequestMapping(value = "/getAll", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//    public ResponseEntity<List<Department>> getAllDepartment(){
+//        List<Department> departments = departmentService.getAll();
+//        if (departments.isEmpty()) {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//        return new ResponseEntity<>(departments,HttpStatus.OK);
+//    }
+//
+//
+//
+//    @RequestMapping(value = "/add", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//    public ResponseEntity<Department> saveDepartment(@RequestBody @Valid Department department, UriComponentsBuilder builder){
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//
+//        if (department == null){
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+//        departmentService.save(department);
+//
+//        httpHeaders.setLocation(builder.path("/department/getAll").buildAndExpand().toUri());
+//        return new ResponseEntity<>(department,httpHeaders,HttpStatus.CREATED);
+//    }
 
 
 }
