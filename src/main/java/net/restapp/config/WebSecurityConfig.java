043d590d6e58bee76389059a52eda9bbf78969f8
mexmatanka <svitlana.anulich@gmail.com>
2018-03-07 17:38:13 +0200

    change Authentication to use basicAuthenticationEntryPoint

--- src/main/java/net/restapp/config/WebSecurityConfig.java
+++ src/main/java/net/restapp/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
-package net.restapp;
+package net.restapp.config;
 
+import net.restapp.security.CustomBasicAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -22,10 +24,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.authorizeRequests()
-                //.antMatchers("/**").permitAll()
                 .antMatchers("/**").authenticated()
-                .and().httpBasic()
+                //.antMatchers("/**").permitAll()
+                .and()
+                .httpBasic().realmName("REALM").authenticationEntryPoint(basicAuthenticationEntryPoint())
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().csrf().disable();
 
@@ -39,5 +43,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+
+    @Bean
+    public BasicAuthenticationEntryPoint basicAuthenticationEntryPoint() {
+        return new CustomBasicAuthenticationEntryPoint();
     }
 }
--- src/main/java/net/restapp/servise/UserDetailServiceImpl.java
+++ src/main/java/net/restapp/servise/UserDetailServiceImpl.java
@@ -1,33 +1,37 @@
 package net.restapp.servise;
 
+import lombok.NonNull;
 import net.restapp.model.User;
 import net.restapp.repository.RepoUser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
-public class UserDetailService implements UserDetailsService {
+public class UserDetailServiceImpl implements UserDetailsService {
 
     @Autowired
     RepoUser repoUser;
 
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = repoUser.findByEmail(email);
-        if (user == null) throw new UsernameNotFoundException("There is no user with email: " + email);
-        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
 
+    @Override
+    public UserDetails loadUserByUsername(@NonNull String email) throws UsernameNotFoundException {
+        User user = repoUser.findByEmail(email);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(email);
+        }
+
+        List<SimpleGrantedAuthority> grantedAuthorities = new ArrayList<>();
         grantedAuthorities.add(new SimpleGrantedAuthority(user.getRole().getName()));
 
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), grantedAuthorities);
+
     }
 }
-
