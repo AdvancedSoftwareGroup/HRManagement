9465aa4e0e4376098c95a6cd7fb34b34a9744c01
DimaKrasin <ktasindim@gmail.com>
2018-03-09 20:53:21 +0200

    add workingHours controller, Email and PDF functional, email sending in Sheduler Task

---
+++ src/main/java/net/restapp/repository/RepoWorkingHours.java
@@ -0,0 +1,20 @@
+package net.restapp.repository;
+
+import net.restapp.model.Employees;
+import net.restapp.model.WorkingHours;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface RepoWorkingHours extends JpaRepository<WorkingHours, Long> {
+    @Query("select wh.employees FROM WorkingHours wh where wh.startTime = ?1")
+    List<Employees> findAllEmployeeForDate(Date date);
+
+    @Query("select em FROM Employees em where em.id = ?1")
+    Integer getAvailableVacationDay(Long id);
+
+}
